a2b9ba77256b3a9b0eb8e1438fb2a83e905759d2
Daniel Succar <danielsuccar@icloud.com>
2020-01-22 23:37:53 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@
 # }
 
 def naming_system
-  naming_system ={
+  naming_system = {
       label: "Kingdom",
       sub_category: {
         label: "Phylum",
@@ -43,7 +43,7 @@ def naming_system
         }
       }
     }
-  
+
 
 
   # Remember:
